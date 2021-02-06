6006c793ee272ef287ef6aa3d390fb4016013bc2
Sun971 <aisorya.bhandari@gmail.com>
2021-02-06 16:29:27 -0500

    Adding all files

---
+++ starter/background.js
@@ -0,0 +1,16 @@
+var pageConditions = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {
+        schemes: ["https", "http"],
+      },
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()],
+};
+// rule for when our app can be used
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([]);
+  });
+});
